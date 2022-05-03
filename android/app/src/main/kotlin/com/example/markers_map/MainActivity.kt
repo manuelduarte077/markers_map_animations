8648d56d7b86a32598f9fcd50722a27286fa42da
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-03 09:54:04 -0600

    initial commit :tada:

---
+++ android/app/src/main/kotlin/com/example/markers_map/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.markers_map
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
